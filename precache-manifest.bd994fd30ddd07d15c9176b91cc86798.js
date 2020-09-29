2664e732fddf05e46247d9b920dc855c69cd50af
iTNTPiston <zhaoyizhu2000@gmail.com>
2020-09-28 21:02:24 -0400

    test deployment

---
+++ precache-manifest.bd994fd30ddd07d15c9176b91cc86798.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6efc6b2949cfa29881890c18457067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70cfca444db188ebfc4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa461e0d1abeed2ca83e",
+    "url": "/static/js/2.19d0ff8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19d0ff8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70cfca444db188ebfc4",
+    "url": "/static/js/main.3fe58978.chunk.js"
+  },
+  {
+    "revision": "002c5c283b508b16fe4f",
+    "url": "/static/js/runtime-main.e059dd22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
